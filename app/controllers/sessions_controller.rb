3ea2aef47339170927383e2bbce9b44b69111409
ThanhNguyenUIT <thanhnt.uit@gmail.com>
2018-05-30 15:29:00 +0700

    Add account activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,15 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by email: params[:session][:email].downcase
     if @user && @user.authenticate(params[:session][:password])
-      log_in @user
-      params[:session][:remember_me] == Settings.remember_me.checked ? remember(@user) : forget(@user)
-      redirect_back_or @user
+      if @user.activated?
+        log_in @user
+        params[:session][:remember_me] == Settings.remember_me.checked ?
+                                          remember(@user) : forget(@user)
+        redirect_back_or @user
+      else
+        flash[:warning] = t "sessions.new.not_activated"
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = t "sessions.new.invalid_email_password"
       render :new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      log_in @user
-      flash[:success] = t "layouts.application.welcome"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = t "users.new.check_email"
+      redirect_to root_url
     else
       render :new
     end
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.paginate page: params[:page]
+    @users = User.activated.paginate page: params[:page]
   end
 
   def new
@@ -36,6 +36,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    redirect_to root_url unless @user.activated?
   end
 
   def update
@@ -52,20 +53,17 @@ class UsersController < ApplicationController
     redirect_to root_url unless current_user.admin?
   end
 
-  private
   def correct_user
     load_user
     redirect_to root_url unless current_user? @user
   end
 
-  private
   def load_user
     return if (@user = User.find_by id: params[:id])
     flash[:danger] = t "shared.error_messages.couldnt_found", id: params[:id]
     redirect_to users_url
   end
 
-  private
   def logged_in_user
     unless logged_in?
       store_location
@@ -74,7 +72,6 @@ class UsersController < ApplicationController
     end
   end
 
-  private
   def user_params
     params.require(:user).permit :name, :email, :password,
                                  :password_confirmation
