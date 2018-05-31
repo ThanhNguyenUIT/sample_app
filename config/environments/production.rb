3703faf83fdbb4705b7bbfd646a28fa5c9849f35
ThanhNguyenUIT <thanhnt.uit@gmail.com>
2018-05-31 09:57:33 +0700

    Add mailer authenticate info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,11 +73,11 @@ Rails.application.configure do
   host = "frozen-journey-41693.herokuapp.com"
   config.action_mailer.default_url_options = {host: host}
   ActionMailer::Base.smtp_settings = {
-    :address        => "smtp.sendgrid.net",
+    :address        => "smtp.gmail.com",
     :port           => "587",
     :authentication => :plain,
-    :user_name      => ENV["SENDGRID_USERNAME"],
-    :password       => ENV["SENDGRID_PASSWORD"],
+    :user_name      => ENV["hoareus194@gmail.com"],
+    :password       => ENV["396112100Hoa"],
     :domain         => "heroku.com",
     :enable_starttls_auto => true
   }
